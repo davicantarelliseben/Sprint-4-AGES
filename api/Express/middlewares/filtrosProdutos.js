c4396ccbb299096969b1e7e35ae49b4a0097dbc4
BaltazarDostatny <dostatny.dev@gmail.com>
2026-04-30 16:43:50 -0300

    Add produtos page

--- api/Express/middlewares/filtrosProdutos.js
+++ api/Express/middlewares/filtrosProdutos.js
@@ -1,11 +1,15 @@
 function filtrosProdutos(query) {
     let filtros = {};
 
+    // FILTROS EXISTENTES
     if(query.id) {filtros._id = query.id};
     if(query.marca) {filtros.marca = query.marca};
     if(query.tamanho) {filtros.tamanho = query.tamanho};
     if(query.cor) {filtros.cor = query.cor};
     if(query.genero) {filtros.genero = query.genero};
+
+    // FILTROS INEXISTENTES
+    
 
     return filtros;
 };
